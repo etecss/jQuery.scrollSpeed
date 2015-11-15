9468e877c84bddfcf5f2636b494fe3c1d27b9eb9
Nicolas <etecss@users.noreply.github.com>
2015-11-15 20:57:43 +0100

    Update jQuery.scrollSpeed.js
    removed horizontal scroll
    no more conflict with other scroll source (scroll top button for example)

--- jQuery.scrollSpeed.js
+++ jQuery.scrollSpeed.js
@@ -1,97 +1,39 @@
-// Plugin: jQuery.scrollSpeed
-// Source: github.com/nathco/jQuery.scrollSpeed
-// Author: Nathan Rutzky
-// Update: 1.0.2
-
+// Custom scrolling speed with jQuery
 (function($) {
-    
     jQuery.scrollSpeed = function(step, speed, easing) {
-        
-        var $document = $(document),
-            $window = $(window),
-            $body = $('html, body'),
-            option = easing || 'default',
-            root = 0,
+        var option = easing || 'default',
+            root = pageYOffset,
             scroll = false,
-            scrollY,
-            scrollX,
             view;
-            
-        if (window.navigator.msPointerEnabled)
-        
-            return false;
-            
-        $window.on('mousewheel DOMMouseScroll', function(e) {
-            
+        $(window).on('wheel', function(e) {
             var deltaY = e.originalEvent.wheelDeltaY,
-                detail = e.originalEvent.detail;
-                scrollY = $document.height() > $window.height();
-                scrollX = $document.width() > $window.width();
-                scroll = true;
-            
-            if (scrollY) {
-                
-                view = $window.height();
-                    
-                if (deltaY < 0 || detail > 0)
-            
-                    root = (root + view) >= $document.height() ? root : root += step;
-                
-                if (deltaY > 0 || detail < 0)
-            
-                    root = root <= 0 ? 0 : root -= step;
-                
-                $body.stop().animate({
-            
+                view = $(window).height();
+                if (!scroll){
+                	scroll = true;
+                	//scroll DOWN
+                    if (deltaY < 0)
+                    	root = (pageYOffset + step > $(document).height() - view) ? $(document).height() - view : pageYOffset + step;
+                    //scroll UP
+                    if (deltaY > 0)
+                    	root = (pageYOffset - step < 0) ? 0 : pageYOffset - step;
+                }else{
+                	//On multiple scroll
+                	//scroll DOWN
+                    if (deltaY < 0)
+                    	root = (root + step > $(document).height() - view) ? $(document).height() - view : root + step;
+                    //scroll UP
+                    if (deltaY > 0)
+                    	root = (root - step < 0) ? 0 : root - step;
+                }
+                $('html, body').stop().animate({
                     scrollTop: root
-                
-                }, speed, option, function() {
-            
-                    scroll = false;
-                
+                }, speed, option, function(){
+                	scroll = false;
                 });
-            }
-            
-            if (scrollX) {
-                
-                view = $window.width();
-                    
-                if (deltaY < 0 || detail > 0)
-            
-                    root = (root + view) >= $document.width() ? root : root += step;
-                
-                if (deltaY > 0 || detail < 0)
-            
-                    root = root <= 0 ? 0 : root -= step;
-                
-                $body.stop().animate({
-            
-                    scrollLeft: root
-                
-                }, speed, option, function() {
-            
-                    scroll = false;
-                
-                });
-            }
-            
             return false;
-            
-        }).on('scroll', function() {
-            
-            if (scrollY && !scroll) root = $window.scrollTop();
-            if (scrollX && !scroll) root = $window.scrollLeft();
-            
-        }).on('resize', function() {
-            
-            if (scrollY && !scroll) view = $window.height();
-            if (scrollX && !scroll) view = $window.width();
-            
         });       
     };
-    
     jQuery.easing.default = function (x,t,b,c,d) {
-    
         return -c * ((t=t/d-1)*t*t*t - 1) + b;
     };
     
